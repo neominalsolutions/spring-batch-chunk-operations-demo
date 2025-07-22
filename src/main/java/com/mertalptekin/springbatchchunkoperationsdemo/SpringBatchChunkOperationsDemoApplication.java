c07d7893da7aa894312b62998b07b06419856491
neominal.solutions <neominal.solutions@gmail.com>
2025-07-22 11:32:02 +0300

    Initial commit

---
+++ src/main/java/com/mertalptekin/springbatchchunkoperationsdemo/SpringBatchChunkOperationsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mertalptekin.springbatchchunkoperationsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchChunkOperationsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchChunkOperationsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mertalptekin/springbatchchunkoperationsdemo/SpringBatchChunkOperationsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mertalptekin.springbatchchunkoperationsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchChunkOperationsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
